1a5abe0c4768f264e75fc180bbf7e1693a7ee94e
DaniiiGarcia <Dagarlow@gmail.com>
2024-10-18 11:31:22 +0200

    Todos los DTO creados

--- src/Autor/DTO_Autor.java
+++ src/Autor/DTO_Autor.java
@@ -1,6 +1,8 @@
 package Autor;
 
-public class DTO_Autor {
+import java.util.Objects;
+
+public class DTO_Autor{
 
     private Integer id;
     private String Nombre;
@@ -9,4 +11,42 @@ public class DTO_Autor {
         this.id = id;
         Nombre = nombre;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DTO_Autor dtoAutor = (DTO_Autor) o;
+        return Objects.equals(id, dtoAutor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "DTO_Autor{" +
+                "id=" + id +
+                ", Nombre='" + Nombre + '\'' +
+                '}';
+    }
+
 }
