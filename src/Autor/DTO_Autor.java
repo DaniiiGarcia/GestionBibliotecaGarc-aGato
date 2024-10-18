fe86e10fcbb945e4270d0f1603d9d75bb316ad77
DaniiiGarcia <Dagarlow@gmail.com>
2024-10-18 10:52:17 +0200

    Creamos la base de datos, he tocado la clase JDBC

--- src/Autor/DTO_Autor.java
+++ src/Autor/DTO_Autor.java
@@ -1,4 +1,12 @@
 package Autor;
 
 public class DTO_Autor {
+
+    private Integer id;
+    private String Nombre;
+
+    public DTO_Autor(Integer id, String nombre) {
+        this.id = id;
+        Nombre = nombre;
+    }
 }
--- src/Libro/DTO_Libro.java
+++ src/Libro/DTO_Libro.java
@@ -1,4 +1,66 @@
 package Libro;
 
+import java.util.Objects;
+
 public class DTO_Libro {
+    private Integer id;
+    private String titulo;
+    private String ISBN;
+
+    public DTO_Libro(Integer id, String titulo, String ISBN) {
+        this.id = id;
+        this.titulo = titulo;
+        this.ISBN = ISBN;
+    }
+
+    public DTO_Libro(String titulo, String ISBN) {
+        this.titulo = titulo;
+        this.ISBN = ISBN;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DTO_Libro dtoLibro = (DTO_Libro) o;
+        return Objects.equals(id, dtoLibro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "DTO_Libro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                '}';
+    }
 }
