4051abec6a4859e890ecfcedb69c85ae50f2aa13
DaniiiGarcia <Dagarlow@gmail.com>
2024-10-18 12:14:41 +0200

    Commit de la clase DAO_Autor, he creado el metodo  insert(); y addAutor(1, "Daniel");

--- src/Autor/DAO_Autor.java
+++ src/Autor/DAO_Autor.java
@@ -3,8 +3,10 @@ package Autor;
 import BBDD.JDBC;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class DAO_Autor {
 
@@ -12,10 +14,12 @@ public class DAO_Autor {
     JDBC jdbc = new JDBC();
     public DAO_Autor() throws SQLException {
         conexion = jdbc.getConextion();
-        create();
+        createAutor();
+        insert();
+        addAutor(1, "Daniel");
     }
 
-    public void create(){
+    public void createAutor(){
         String CREATE_AUTOR= """
                 CREATE TABLE IF NOT EXISTS Autor (
                     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -32,4 +36,42 @@ public class DAO_Autor {
             System.out.println("Error al crear la tabla del usuario");
         }
     }
+
+    public void insert(){
+        String insert = """
+                INSERT INTO Autor (nombre)
+                VALUES
+                    ('Gabriel García Márquez'),
+                    ('Isabel Allende'),
+                    ('Mario Vargas Llosa'),
+                    ('Jorge Luis Borges'),
+                    ('Julio Cortázar');
+                """;
+
+        try (Statement s = conexion.createStatement()){
+            s.executeUpdate(insert);
+            System.out.println("Tabla Autor creada con exito");
+
+        } catch (SQLException e) {
+            System.out.println("Error al crear la tabla Autor");
+        }
+    }
+
+    public void addAutor(Integer id, String Nombre){
+        String add = "INSERT INTO Autor values(?,?)";
+
+        try (PreparedStatement ps = conexion.prepareStatement(add)){
+
+            System.out.println("Introduzca un nombre");
+            ps.setInt(1, id);
+            ps.setString(2, Nombre);
+            ps.executeUpdate();
+            System.out.println("Autor creado con exito");
+
+        } catch (SQLException e) {
+            System.out.println("Error al crear la tabla Autor");
+        }
+    }
 }
+
+
