e50a8682579bd8392958fffb4f4c46add0245b36
DaniiiGarcia <Dagarlow@gmail.com>
2024-10-21 20:58:22 +0200

    DAO Libro actualizado

--- src/Libro/DAO_Libro.java
+++ src/Libro/DAO_Libro.java
@@ -36,13 +36,14 @@ public class DAO_Libro {
         }
     }
 
-    public void addLibro(String titulo, String ISBN) {
-        String add = "INSERT INTO Autor values(?,?)";
+    public void addLibro(int id, String titulo, String ISBN) {
+        String add = "INSERT INTO Libro values(?,?,?)";
 
         try (PreparedStatement ps = conexion.prepareStatement(add)) {
 
-            ps.setString(1, titulo);
-            ps.setString(2, ISBN);
+            ps.setInt(1,id);
+            ps.setString(2, titulo);
+            ps.setString(3, ISBN);
             ps.executeUpdate();
             System.out.println("Libro creado con exito");
 
@@ -50,4 +51,30 @@ public class DAO_Libro {
             System.out.println("Error al crear el Libro");
         }
     }
+
+    public void eliminarLibro(int id) {
+        String del = "DELETE FROM Libro WHERE id = ?";
+
+        try (PreparedStatement ps = conexion.prepareStatement(del)) {
+            ps.setInt(1, id);
+            ps.execute();
+            System.out.println("Libro eliminado con exito");
+        } catch (SQLException e) {
+            System.out.println("Error al borrar el libro");
+        }
+    }
+
+
+    public void modificarLibro(Integer id, String titulo, String isbn) {
+        String mod = "UPDATE Libro SET titulo = ? WHERE id = ?";
+        try (PreparedStatement ps = conexion.prepareStatement(mod)) {
+            ps.setString(1, titulo);
+            ps.execute();
+            System.out.println("Autor modificado con exito");
+        } catch (SQLException e) {
+            System.out.println("Error al modificar el autor");
+        }
+    }
+
+
 }
