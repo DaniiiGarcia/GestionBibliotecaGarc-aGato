85fd55fdc160cfd37e3c1b5332871d095fb76772
DaniiiGarcia <Dagarlow@gmail.com>
2024-10-23 09:03:46 +0200

    CLASE DTO CON EL NUEVO TO STRING

--- src/Autor/DTO_Autor.java
+++ src/Autor/DTO_Autor.java
@@ -7,6 +7,7 @@ public class DTO_Autor{
 
     private Integer id;
     private String Nombre;
+    private static int cont = 0;
 
     public DTO_Autor(Integer id, String nombre) {
         this.id = id;
@@ -44,21 +45,26 @@ public class DTO_Autor{
 
     @Override
     public String toString() {
-        return String.format(
-                """
-                +-----------+------------------------+
-                |    ID     |         Nombre         |
-                +-----------+------------------------+
+
+        if (cont == 0) {
+            System.out.println("+-----------+------------------------+");
+            System.out.println("|    ID     |         Nombre         |");
+            System.out.println("+-----------+------------------------+");
+        }
+        cont ++;
+        return String.format("""
                 |  %-9s| %-22s |
-                +-----------+------------------------+
-                """,
+                +-----------+------------------------+""",
                 this.id,
                 this.Nombre);
     }
 
     public static void main(String[] args) {
-        DTO_Autor auor = new DTO_Autor(1, "sedfhujkdcfbj ");
+        DTO_Autor auor = new DTO_Autor(1, "Miguel_de_Cervantes ");
+        DTO_Autor auTor = new DTO_Autor(2, "Shakespeare ");
+
         System.out.println(auor);
+        System.out.println(auTor);
     }
 
 }
